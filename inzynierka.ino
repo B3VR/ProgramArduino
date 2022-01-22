c1481239679982de22f7c084a9eb579907e908a5
B3VR <73071619+B3VR@users.noreply.github.com>
2022-01-22 20:05:54 +0100

    ogarnięte komunikaty ale nie działa nagłówek

--- inzynierka.ino
+++ inzynierka.ino
@@ -1,13 +1,15 @@
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <SD.h>
+//#include "SdFat.h"
 
 bool isMeasurmetInProgress = false;
 int buttonPin = A1;
 int buttonNew;
 int buttonOld = 1;
 // ECG sensor
-bool wasElectrodeDisconneted = false;
+bool wasElectrodeDisconnected = false;
+bool isElectrodeDisconnected = false;
 int l0MPin = A2;
 int l0PPin = A3;
 int signalPin = A5;
@@ -15,7 +17,10 @@ int signalPin = A5;
 const int rs = 8, en = 7, d4 = 2, d5 = 3, d6 = 5, d7 = 6;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 // SD card
+//SdFat SD;
 File signalFile;
+int csPin = 4;
+
 String fileName = "EKG";
 
 void setup() {
@@ -60,7 +65,7 @@ void checkButtonPush() {
 }
 
 bool beginMeasurement() {
-  if (!SD.begin(4)) {
+  if (!SD.begin(csPin)) {
     SDissueMessage();
     delay(1000);
     startMessage();
@@ -81,29 +86,50 @@ bool beginMeasurement() {
     
   unsigned long measurmentTime = millis() - measurmentStartTime;
 
+  delay(1);
   String header = createHeader(measurmentTime, fullFileName);
   signalFile.println(header);
   signalFile.close();
-  wasElectrodeDisconneted = false;
+  wasElectrodeDisconnected = false;
   return true;
 }
 
-void saveSample() {
-  int newL0P = digitalRead(l0PPin);
-  int newL0M = digitalRead(l0MPin);
+void saveSample()  {
+  bool oldIsElectrodeDisconnected = isElectrodeDisconnected;
+
+  int L0P = digitalRead(l0PPin);
+  int L0M = digitalRead(l0MPin);
   
-  if (newL0M == 1 || newL0P == 1) {
-    disconnectedElectrodeMessage();
-    wasElectrodeDisconneted = true;
+  if (L0M == 1 || L0P == 1) {
+    wasElectrodeDisconnected = true;
+    isElectrodeDisconnected = true;
 
   }else {
-    measurmentInProgressMessage();
+    isElectrodeDisconnected = false;
     float sample = analogRead(signalPin);
     Serial.println(sample);   
     signalFile.println(sample);
   }
 
+  bool newIsElectrodeDisconnected = isElectrodeDisconnected;
+
+  //checkElectrodeMessage(oldIsElectrodeDisconnected, newIsElectrodeDisconnected);
   checkButtonPush();
+}
+
+void checkElectrodeMessage(bool oldIsElectrodeDisconnected, bool newIsElectrodeDisconnected){
+
+  if (oldIsElectrodeDisconnected != newIsElectrodeDisconnected)
+  {
+    if (newIsElectrodeDisconnected == true)
+    {
+      disconnectedElectrodeMessage();
+
+    }else if (newIsElectrodeDisconnected == false)
+    {
+      measurmentInProgressMessage();
+    }
+  }
 }
 
 // Pomocnicze
@@ -113,7 +139,7 @@ String createHeader(long measurmentTime, String fileName){
   float timeInSec = float(measurmentTime) / 1000;
   header = "Pomiar: " + fileName + " Czas badania: " + timeInSec + " sekund";
 
-  if (wasElectrodeDisconneted)
+  if (wasElectrodeDisconnected)
   {
     header = header + " (Podczas badania odpięto elektrodę ! Sygnał może być niepełny)";
   }
@@ -168,25 +194,3 @@ void SDissueMessage() {
   lcd.setCursor(0,1);
   lcd.print("pamieci !");
 }
-
-// void test() {
-//   SD.begin(4);
-  
-//   String fullFileName = getNextFileName();  
-//   signalFile = SD.open(fullFileName, FILE_WRITE);
-//   signalFile = SD.open(fullFileName, FILE_WRITE);
-
-//   unsigned long measurmentStartTime = millis();
-//   unsigned long koniecBadania = 0;
-//   while (koniecBadania <= 10000)
-//   {
-//     saveSample();
-//     koniecBadania = millis() - measurmentStartTime;
-//   }
-  
-//   String header = createHeader(koniecBadania);
-//   signalFile.println(header);
-//   signalFile.close();
-//   wasElectrodeDisconneted = false;
-
-// }
